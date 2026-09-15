d9a82536683613c2273cbb4f1e7ae594264ff0da
Laban <317085441+laban-endji@users.noreply.github.com>
2026-09-15 14:19:40 +0100

    Create a function called operate

--- js/script.js
+++ js/script.js
@@ -17,3 +17,24 @@ function divide(a, b) {
 let firstNumber = 0;
 let secondNumber = 0;
 let operator = "";
+
+function operate(operator, a, b) {
+    let result = 0;
+
+    switch(operator) {
+        case "+":
+            result = add(a,b);
+            break;
+        case "-":
+            result = subtract(a,b);
+            break;
+        case "*":
+            result = multiply(a,b);
+            break;
+        case "/":
+            result = divide(a,b);
+            break;
+    }
+    
+    return result;
+}

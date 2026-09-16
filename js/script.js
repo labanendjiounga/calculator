14bd71271a051c228ae73976e1597b18de31d8f6
Laban <317085441+laban-endji@users.noreply.github.com>
2026-09-16 09:00:52 +0100

    Store inputted numbers and update display

--- js/script.js
+++ js/script.js
@@ -14,8 +14,8 @@ function divide(a, b) {
   return a / b;
 }
 
-let firstNumber = 0;
-let secondNumber = 0;
+let firstNumber = "";
+let secondNumber = "";
 let operator = "";
 
 function operate(operator, a, b) {
@@ -38,3 +38,46 @@ function operate(operator, a, b) {
   
   return result;
 }
+
+function updateOperator(sign) {
+  if(sign == "÷") {
+    operator = "/";
+  } else {
+    operator = sign;
+  }
+}
+
+function updateDisplay(text) {
+  const display = document.querySelector(".display");
+  display.textContent = "";
+  display.textContent = text;
+}
+
+function updateNumbers(number) {
+  if(operator == "") {
+    firstNumber += number;
+    updateDisplay(firstNumber);
+  } else {
+    secondNumber += number;
+    updateDisplay(secondNumber);
+  }
+}
+
+const buttons = document.querySelectorAll("button");
+buttons.forEach(button => {
+  button.addEventListener("click", e => {
+    const button = e.target;
+    
+    if(firstNumber == "" && secondNumber == "" && button.textContent == "0") {
+      return;
+    }
+
+    if("+-x÷".includes(button.textContent)) {
+      updateOperator(button.textContent);
+    }
+    
+    if("0123456789".includes(button.textContent)) {
+      updateNumbers(button.textContent);
+    }
+  });
+});
